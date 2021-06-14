15ffff284b4424285101bbd19412572091e7e626
Atishek Kumar <kumaratishek22@gmail.com>
2021-06-14 13:54:43 +0000

    Init

---
+++ models/teams.model.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const TeamSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  // members: {
+  //   type: [
+  //     {
+  //       type: mongoose.Schema.Types.ObjectId,
+  //       ref: "User",
+  //     },
+  //   ],
+  // },
+  description: String,
+});
+module.exports = {
+  TeamSchema,
+};
